73c9ee3ea8a7ab1ea346b4ea98a59892ef29418b
blusanders <blusanders@yahoo.com>
2021-06-13 09:15:46 -0500

    Simple leaflet map render

--- src/hold.js
+++ src/hold.js
@@ -1,13 +1,29 @@
+import logo from './logo.svg';
 import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Give Your Strength
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import * as parkData from "./data/skateboard-parks.json";
-
+import "./App.css";
 
 export default function App() {
   return (
-    <MapContainer center={[36.179150, -86.759080]} zoom={12}>
+    <MapContainer center={[45.4, -75.7]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -15,3 +31,5 @@ export default function App() {
     </MapContainer>
   );
 }
+
+
